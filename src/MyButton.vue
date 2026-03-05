56798e4df01343c6b688cacc9d40d84ed5521598
Mathew Thompson <work@Mathews-MacBook-Air.local>
2026-03-05 10:52:53 -0600

    Added new project

---
+++ src/MyButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const { type } = defineProps<{
+  type?: "primary";
+}>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <button class="my-button" @click="count++">
+    my button<br />
+    type: {{ type }}<br />
+    count: {{ count }}
+  </button>
+</template>
+
+<style></style>
